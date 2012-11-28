96ac09a898a928a1866d262384e2c5dda36dc921
Jason Marshall <ctrlspc@gmail.com>
2012-11-28 16:44:50 +0000

    Created the skeleton of the project including a reasonably
    detailed README file.

---
+++ bin/rmas_adapter_admin.py
@@ -0,0 +1,7 @@
+'''
+This is the script that you will use to create your new rmas_adapter skeleton.
+It will use the adapter_template to create a new adapter, which provides you with
+the runner script that allows you to start your adapter polling, and the settings file
+where you specify the connection details to connect to the rmas bus, and which rmas events
+you want to respond to.
+'''
