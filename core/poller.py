95b6f8ccba05a38c1b0bf770e692c52eb5037f1a
Jason Marshall <ctrlspc@gmail.com>
2012-11-29 14:14:03 +0000

    added functionality to the poller so that it passes the events
    to registered event handlers.

--- core/poller.py
+++ core/poller.py
@@ -9,7 +9,9 @@ import datetime
 import logging
 from core.rmas_bus import RMASBus
 from conf import settings
+from core.parser import parse_event
 
+import importlib
 
 last_poll = None
 bus = None
@@ -26,19 +28,43 @@ def start_polling():
     bus = RMASBus()
     poll_for_events()
     
+def _get_handlers(event_type):
+    '''
+        Attempts to get any event handlers based on the settings.EVENTS list
+        If none are found it will return an empty list.
         
+        Returns:
+            A list of event handling modules (which will have a function called handle that accepts the
+            payload of the event) or an empty list if there are no handlers registered.
+    '''
     
+    handlers = []
+    
+    for event in settings.EVENTS:
+        #each event will be a tuple, the first element is the event type, the second element is the path to the handler module
+        if event[0] == event_type:
+            #attempt to import the handler module
+            handlers.append(importlib.import_module(event[1]))
+    
+    return handlers
 
 def poll_for_events():
-    logging.info('Polling ESB')
+    logging.debug('Polling ESB')
     
     global last_poll
     events = bus.get_events(last_poll.isoformat())
     last_poll = datetime.datetime.now()
     
-    for event in events:
-        #handle the events as described in the settings.EVENTS list
-        logging.info('Got event: %s' % event)
+    if hasattr(events, 'string'):
+        for event in events.string:
+   
+            #handle the events as described in the settings.EVENTS list
+            event_type, payload = parse_event(event)
+            logging.debug ('Got an event of type: %s' % event_type)
+            
+            for handler in _get_handlers(event_type):
+                handler.handle_event(payload=payload)
+            
     #update the last_poll time - we only want new events after this time.
 
     Timer(settings.POLL_INTERVAL/1000, poll_for_events).start()#poll again in 2 seconds time!
