d8f6bb2b5c46a0d328ba982c36b91c913a938193
Jason Marshall <ctrlspc@gmail.com>
2012-11-29 16:28:31 +0000

    removed the adapter_name prefix and fixed import

--- rmas_adapter/conf/adapter_template/runner.py
+++ rmas_adapter/conf/adapter_template/runner.py
@@ -4,9 +4,9 @@ import logging
 if __name__ == '__main__':
 
     #this has to come first before you import any other modules otherwsise the settings won't be intitialized
-    os.environ.setdefault("RMAS_ADAPTER_SETTINGS", "%(adapter_name)s.settings")
+    os.environ.setdefault("RMAS_ADAPTER_SETTINGS", "settings")
     
     logging.basicConfig(level=logging.INFO)
     
-    from core.poller import start_polling
+    from rmas_adapter.core.poller import start_polling
     start_polling()
