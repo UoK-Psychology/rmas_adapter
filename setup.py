8d5d755b16faf84f41f682afe40d6325809b3531
Jason Marshall <ctrlspc@gmail.com>
2012-12-20 12:13:24 +0000

    anotehr verison increment to push the bug fiexes up to
    PyPi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='RMASAdapter',
-    version='0.1.2',
+    version='0.1.3',
     author='Jason Marshall',
     author_email='j.j.marshall@kent.ac.uk',
     packages=['rmas_adapter', 
